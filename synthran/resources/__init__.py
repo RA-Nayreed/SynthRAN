fe1e1aa873e933c0e8dab45335f5079059a89c93
Nayreed <87903538+RA-Nayreed@users.noreply.github.com>
2026-08-17 21:49:07 +0000

    Expose composite resource transaction services

--- synthran/resources/__init__.py
+++ synthran/resources/__init__.py
@@ -18,20 +18,36 @@ from synthran.resources.requirements import (
     requirements_from_desired,
 )
 from synthran.resources.selector import select_resources
+from synthran.resources.transaction import (
+    AcquisitionReceipt,
+    ProviderTransactionRecord,
+    ReleaseReceipt,
+    ResourceProviderAdapter,
+    ResourceTransactionError,
+    ResourceTransactionResult,
+    execute_resource_transaction,
+)
 
 __all__ = [
+    "AcquisitionReceipt",
     "ProviderResourceSet",
     "ProviderResourceSnapshot",
+    "ProviderTransactionRecord",
+    "ReleaseReceipt",
     "ResourceAssignment",
     "ResourceDecision",
     "ResourceDescriptor",
     "ResourceInventory",
+    "ResourceProviderAdapter",
     "ResourceRequirement",
     "ResourceSelection",
     "ResourceSelectionError",
     "ResourceState",
+    "ResourceTransactionError",
+    "ResourceTransactionResult",
     "build_resource_decision",
     "effective_radio_backend",
+    "execute_resource_transaction",
     "requirements_from_desired",
     "reviewed_resource_descriptors",
     "select_resources",
